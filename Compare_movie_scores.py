2bdc00f66e7d2fb343d3f433a4470faf916dd130
edytabar <37018120+edytabar@users.noreply.github.com>
2020-12-19 22:06:47 +0100

    Update Compare_movie_scores.py

--- Compare_movie_scores.py
+++ Compare_movie_scores.py
@@ -6,7 +6,8 @@ Reworked version of alghorithms from the book:
 Artificial Intelligence with Python by Prateek Joshi
 
 This program compares each user ratings of movies stored in CSV file.
-There are 3 alternative functions to recommend the best movies.
+There are 3 alternative functions to recommend the best movies (euclidean,
+pearson and our own- edmar).
 This program by default will compare all users to the declared user1. 
 
 In the main function you can change user and/or function.
@@ -17,8 +18,8 @@ import pandas as pd
 import numpy as np
 
 
-## Compute the Euclidean distance score between user1 and user2 
 def euclidean_score(dataset, user1, user2):
+    '''Compute the Euclidean distance score between user1 and user2'''
     if user1 not in dataset:
         raise TypeError('Cannot find ' + user1 + ' in the dataset')
 
@@ -31,7 +32,7 @@ def euclidean_score(dataset, user1, user2):
     for item in dataset[user1]:
         if item in dataset[user2]:
             common_movies[item] = 1
-    print(common_movies)
+    #print(common_movies)
     
     ## If there are no common movies between the users, then the score is 0 
     if len(common_movies) == 0:
@@ -41,14 +42,17 @@ def euclidean_score(dataset, user1, user2):
 
     for item in dataset[user1]:
         if item in dataset[user2]:
+            print(item, " ", dataset[user1][item], ":", dataset[user2][item])
             squared_diff.append(np.square(dataset[user1][item] 
                                           - dataset[user2][item]))
+    result = 1 / (1 + np.sqrt(np.sum(squared_diff)))
 
-    return 1 / (1 + np.sqrt(np.sum(squared_diff))) 
+    return "%.2f" % result 
 
 
-## Compute the Pearson correlation score between user1 and user2 
+
 def pearson_score(dataset, user1, user2):
+    '''Compute the Pearson correlation score between user1 and user2'''
     if user1 not in dataset:
         raise TypeError('Cannot find ' + user1 + ' in the dataset')
 
@@ -71,8 +75,7 @@ def pearson_score(dataset, user1, user2):
     ## Calculate the sum of ratings of all the common movies 
     user1_sum = np.sum([dataset[user1][item] for item in common_movies])
     user2_sum = np.sum([dataset[user2][item] for item in common_movies])
-    #print(user1_sum)
-    #print(user2_sum)
+
     
     ## Calculate the sum of squares of ratings of all the common movies 
     user1_squared_sum = np.sum([np.square(dataset[user1][item]) for 
@@ -83,23 +86,26 @@ def pearson_score(dataset, user1, user2):
     ## Calculate the sum of products of the ratings of the common movies
     sum_of_products = np.sum([dataset[user1][item] * dataset[user2][item] for 
                               item in common_movies])
-    #print(sum_of_products)
     
+    for item in dataset[user1]:
+        if item in dataset[user2]:
+            print(item, " ", dataset[user1][item], ":", dataset[user2][item])
+            
     ## Calculate the Pearson correlation score
     Sxy = abs(sum_of_products - (user1_sum * user2_sum / num_ratings))
     Sxx = abs(user1_squared_sum - np.square(user1_sum) / num_ratings)
     Syy = abs(user2_squared_sum - np.square(user2_sum) / num_ratings)
-    #print(Sxy,Sxx,Syy)
+    
     if Sxx * Syy == 0:
         return 0
-    #print(common_movies)
-
     return Sxy / np.sqrt(Sxx * Syy)
 
 
-    ## our version based on Euclidean distance score between user1 and user2
-    # works better when dataset is small and there is only few commmon items
+
+
 def edmar_score(dataset, user1, user2):
+    '''our version based on Euclidean distance score between user1 and user2
+    works better when dataset is small and there is only few commmon items'''
     if user1 not in dataset:
         raise TypeError('Cannot find ' + user1 + ' in the dataset')
     if user2 not in dataset:
@@ -117,12 +123,14 @@ def edmar_score(dataset, user1, user2):
     if len(common_movies) == 0:
         return 0
 
-
     diff = []
     
     for item in dataset[user1]:
         if item in dataset[user2]:
             print(item, " ", dataset[user1][item], ":", dataset[user2][item])
+            ## For each point of difference between scores of user1 and user2 
+            # the penalty points are counted. Penalty is doubled when comparing 
+            # with highest and lowest scores.
             if dataset[user1][item] < 3:
                 diff.append(40*abs(dataset[user1][item] - dataset[user2][item]))
             elif dataset[user1][item] > 8:
@@ -130,7 +138,6 @@ def edmar_score(dataset, user1, user2):
             else:    
                 diff.append(20*abs(dataset[user1][item] - dataset[user2][item]))
     #print(diff)
-
     import math
     result = math.log10(np.sum(diff)+100/len(common_movies))
     res = 1/(result)*100
@@ -140,7 +147,7 @@ def edmar_score(dataset, user1, user2):
 
 if __name__=='__main__':
     
-    data = pd.read_csv('03.movs_transposed_clean.csv', delimiter=',', 
+    data = pd.read_csv('movs_transposed_clean.csv', delimiter=',', 
                        encoding=('UTF-8'))
     df = pd.DataFrame(data)
     
@@ -150,18 +157,10 @@ if __name__=='__main__':
     for columns in df.columns:
         df[columns] = df[columns].str.lower()
         df[columns] = df[columns].str.title()
-
-    # movs_tmp = [df.iloc[j,i] for i in range(14) for j in range(73) if j%2!=0]
-    # movies = []
-    # for item in movs_tmp:
-    #     if item not in movies:
-    #         movies.append(item)
-    # #print(movies)    
+ 
     
     pairs = [[df.iloc[i,j], df.iloc[i+1,j], df.iloc[0,j]] for i 
-             in range(1,73) if i % 2 != 0 for j in range(14)]
-    #print(pairs)
-    #print(pairs[0][0])
+             in range(1,73,2) for j in range(14)]
     
 
     dataset = {}
@@ -169,14 +168,13 @@ if __name__=='__main__':
         if len(movie) > 1 and movie != "NaN":
             if score != "NaN":
                 dataset.setdefault(user, {})[movie] = int(float(score))
-    #print(dataset)
-    # for k, v in dataset.items():
-    #     print(k,v,'\n')
       
-    
+    ## You can change user1 here:
     user1 = "Paweł Czapiewski"
     user2 = ''
-    score_type = "edmar" # "Euclidean" # "Pearson" # 
+    
+    ## You can change the function here:
+    score_type = "Pearson" # "Euclidean" # "edmar" # 
 
   
     fit = []    
@@ -212,29 +210,43 @@ if __name__=='__main__':
             print()
     
     
-    best_fit = max(fit, key=lambda x: x[1])
-    print("\nHighest probability of match: ", best_fit[0], best_fit[1],"%\n")
+    fit_sorted = sorted(fit, key=lambda x: x[1])
+    worst_fit = fit_sorted[:5]
+    fit_sorted.reverse()
+    best_fit = fit_sorted[:5]
+    
+    #print("\nHighest probability of match: \n", best_fit)
     
     ##Recommended movies
     pcz_movies = []
-    best_fit_user_movies = []
     for movie, score, user in pairs:
-         if user == user1:
-             pcz_movies.append(movie)
-         if user == best_fit[0]:
-             best_fit_user_movies.append(movie)
+        if user == user1:
+            pcz_movies.append(movie)        
     
-    for movie, score in dataset.items():
-        if user == best_fit[0]:
-            if score > 8:
-                    print(movie,'\n')
+    worst_movies = []
+    best_movies = []
+    for [k,v] in best_fit:
+        #userx = k
+        for movie, score, user in pairs:
+            if user == k and score == '10':
+                if movie not in best_movies and movie not in pcz_movies:
+                    best_movies.append(movie)
+            elif user == k and score == '1':
+                if movie not in worst_movies and movie not in pcz_movies:
+                    worst_movies.append(movie)
     
-    print("Recommended movies: ")                        
-    for movie, score, user in pairs:
-         if user == best_fit[0]:
-             if score == '8' or score == '9' or score == '10':
-                 if movie not in pcz_movies: 
-                     print(movie)    
-                 
-                 
+    print("\nRecommended movies:")
+    for x in best_movies[:7]:
+        print("\t", x)
+    
+    print("\nNot recommended movies:")
+    for y in worst_movies[:7]:
+        print("\t", y)
+    
+    # import random    
+    # print("\nRecommended movies:")
+    # print(random.sample(best_movies, 7))
+    # print("\nNot recommended movies:")
+    # print(random.sample(worst_movies, 7))
+             
                  
